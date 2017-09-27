37df78280eeb4dcb30468f3a87887cb86116d546
zm1try <zmitryarosh@gmail.com>
2017-09-27 23:00:39 +0300

    test adding

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
+var mid = 0;
 class GuessingGame {
     constructor() {}
-
     setRange(min, max) {
 
     }
 
     guess() {
-
+        mid = (max - min)/2;
+        return (mid);
     }
 
     lower() {
-
+        min = mid;
     }
 
     greater() {
-
+        max = mid;
     }
 }
 
